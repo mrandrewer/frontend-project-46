a4bba27f8e66d0893f8081f9ab12b52dbff21bbb
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-03 20:33:55 +0300

    Install and init commander.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
-console.log('gendiff');
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program.parse();
