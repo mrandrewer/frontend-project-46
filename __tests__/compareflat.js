fc013b4ba3563ada3d84007732f51de98746556c
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-06 22:13:47 +0300

    Fix linter errors

--- __tests__/compareflat.js
+++ __tests__/compareflat.js
@@ -2,8 +2,7 @@ import { test, expect } from '@jest/globals';
 import gendiff from '../index.js';
 
 test('compare flat files', () => {
-  expect(gendiff('samples/file1.json', 'samples/file2.json')).toBe(
-`{
+  expect(gendiff('samples/file1.json', 'samples/file2.json')).toBe(`{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
