01f143e41794f1962c60a94a72049f35b1744b90
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-06 22:43:41 +0300

    Move test files and test result to fixtures

--- __tests__/compareflat.js
+++ __tests__/compareflat.js
@@ -1,13 +1,17 @@
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'node:fs';
+import path from 'path';
 import gendiff from '../index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const file1 = getFixturePath('flatFile1.json');
+const file2 = getFixturePath('flatFile2.json');
+const expected1 = readFileSync(getFixturePath('flatResultJson.txt'), 'utf8').trimEnd();
+
 test('compare flat files', () => {
-  expect(gendiff('samples/file1.json', 'samples/file2.json')).toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+  expect(gendiff(file1, file2)).toBe(expected1);
 });
