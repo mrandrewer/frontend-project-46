c919da79dc842f17e35c12aba1f8fcfb4ce410ec
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-07 19:16:20 +0300

    Update tests for complex objects in files

--- __tests__/compareyml.js
+++ __tests__/compareyml.js
@@ -8,10 +8,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const file1 = getFixturePath('flatFile1.json');
-const file2 = getFixturePath('flatFile2.json');
-const expected1 = readFileSync(getFixturePath('flatResult.txt'), 'utf8').trimEnd();
+const file1 = getFixturePath('file1.yml');
+const file2 = getFixturePath('file2.yaml');
+const expected1 = readFileSync(getFixturePath('result.txt'), 'utf8').trimEnd();
 
-test('compare flat json files', () => {
+test('compare yaml files', () => {
   expect(gendiff(file1, file2)).toBe(expected1);
 });
