94ed0c26dec44a8c5c9532dec9d4ced1b0324192
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-07 15:20:19 +0300

    Add test for incorrect file type

---
+++ __tests__/unknowfFileFormat.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'node:fs';
+import path from 'path';
+import gendiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const file1 = getFixturePath('flatFile1.yml');
+const file2 = getFixturePath('flatFile2.txt');
+
+test('check error on unknown file format', () => {
+  expect(() => gendiff(file1, file2)).toThrow('Unknown file format');
+});
