dce2fa4290ee94aadea57cf5a4cab19af93a3c07
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-08 21:19:45 +0300

    Satisfy hexlet check try 3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { readFileSync } from 'node:fs';
 import path from 'path';
@@ -14,14 +14,14 @@ describe('test gendiff', () => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     return path.join(__dirname, '..', '__fixtures__', filename);
-  }
-  
+  };
+
   const file1json = getFixturePath('file1.json');
   const file2json = getFixturePath('file2.json');
   const file1yaml = getFixturePath('file1.yml');
   const file2yaml = getFixturePath('file2.yaml');
   const file2txt = getFixturePath('file2.txt');
-  
+
   const expectedStylish = readFileSync(getFixturePath('resultStylish.txt'), 'utf8').trimEnd();
   const expectedPlain = readFileSync(getFixturePath('resultPlain.txt'), 'utf8').trimEnd();
   const expectedJSON = readFileSync(getFixturePath('resultJSON.txt'), 'utf8').trimEnd();
@@ -29,35 +29,35 @@ describe('test gendiff', () => {
   test('compare json files with stylish format', () => {
     expect(gendiff(file1json, file2json, 'stylish')).toBe(expectedStylish);
   });
-  
+
   test('compare json files with plain format', () => {
     expect(gendiff(file1json, file2json, 'plain')).toBe(expectedPlain);
   });
-  
+
   test('compare json files with json format', () => {
     expect(gendiff(file1json, file2json, 'json')).toBe(expectedJSON);
   });
-  
+
   test('compare yaml files with stylish format', () => {
     expect(gendiff(file1yaml, file2yaml, 'stylish')).toBe(expectedStylish);
   });
-  
+
   test('compare yaml files with plain format', () => {
     expect(gendiff(file1yaml, file2yaml, 'plain')).toBe(expectedPlain);
   });
-  
+
   test('compare yaml files with json format', () => {
     expect(gendiff(file1yaml, file2yaml, 'json')).toBe(expectedJSON);
   });
-  
+
   test('compare files with default format', () => {
     expect(gendiff(file1json, file2json)).toBe(expectedStylish);
   });
-  
+
   test('check error on unknown file type', () => {
     expect(() => gendiff(file1json, file2txt)).toThrow('Unknown data type');
   });
-  
+
   test('check error on unknown format', () => {
     expect(() => gendiff(file1json, file2json, 'formatDoesNotExist')).toThrow('Unknown format');
   });
