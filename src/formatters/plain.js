dcddeccb307c0c2790bb32ee7603b6b650f2801d
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-08 18:03:07 +0300

    Fix formatting issues

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,29 +11,27 @@ const getValueStr = (value) => {
 };
 
 const formatPlain = (diff) => {
-  const iter = (records, parent = '') => {
-    return _.orderBy(records, ['key'])
-      .map((elem) => {
-        const fullName = parent === '' ? elem.key : `${parent}.${elem.key}`;
-        switch (elem.type) {
-          case 'add':
-            return `Property '${fullName}' was added with value: ${getValueStr(elem.children[0])}`;
-          case 'remove':
-            return `Property '${fullName}' was removed`;
-          case 'none':
-            return null;
-          case 'update':
-            return `Property '${fullName}' was updated. From ${getValueStr(elem.children[0])} to ${getValueStr(elem.children[1])}`;
-          case 'nested':
-            return iter(elem.children[0], fullName);
-          default:
-            throw new Error('Unknown record type in diff');
-        }
-      })
-      .flat(Infinity)
-      .filter((e) => e !== null)
-      .join('\n');
-  };
+  const iter = (records, parent = '') => _.orderBy(records, ['key'])
+    .map((elem) => {
+      const fullName = parent === '' ? elem.key : `${parent}.${elem.key}`;
+      switch (elem.type) {
+        case 'add':
+          return `Property '${fullName}' was added with value: ${getValueStr(elem.children[0])}`;
+        case 'remove':
+          return `Property '${fullName}' was removed`;
+        case 'none':
+          return null;
+        case 'update':
+          return `Property '${fullName}' was updated. From ${getValueStr(elem.children[0])} to ${getValueStr(elem.children[1])}`;
+        case 'nested':
+          return iter(elem.children[0], fullName);
+        default:
+          throw new Error('Unknown record type in diff');
+      }
+    })
+    .flat(Infinity)
+    .filter((e) => e !== null)
+    .join('\n');
 
   return iter(diff);
 };
--- src/gendiff.js
+++ src/gendiff.js
@@ -6,14 +6,14 @@ import getFormatter from './formatters/index.js';
 
 const readData = (filepath) => {
   const parser = getParser(path.extname(filepath));
-  return parser(readFile(filepath))
-}
+  return parser(readFile(filepath));
+};
 
 const gendiff = (filepath1, filepath2, formatName = 'stylish') => {
   const obj1 = readData(filepath1);
   const obj2 = readData(filepath2);
   const diff = getDiff(obj1, obj2);
-  const formatter = getFormatter(formatName)
+  const formatter = getFormatter(formatName);
   return formatter(diff);
 };
 
