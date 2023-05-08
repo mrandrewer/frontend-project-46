637d96c5f6b086a08253eb82138c4040d7df9fec
Andrei Belskii <mrandrewer@yandex.ru>
2023-05-08 18:34:36 +0300

    Fix coideclimate Cognitive Complexity issue
    Update plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,5 +1,7 @@
 import _ from 'lodash';
 
+const getFullPath = (key, parent) => (parent === '' ? key : `${parent}.${key}`);
+
 const getValueStr = (value) => {
   if (_.isObject(value)) {
     return '[complex value]';
@@ -11,29 +13,28 @@ const getValueStr = (value) => {
 };
 
 const formatPlain = (diff) => {
-  const iter = (records, parent = '') => {
-    const lines = _.orderBy(records, ['key'])
-      .map((elem) => {
-        const fullName = parent === '' ? elem.key : `${parent}.${elem.key}`;
-        switch (elem.type) {
-          case 'add':
-            return `Property '${fullName}' was added with value: ${getValueStr(elem.children[0])}`;
-          case 'remove':
-            return `Property '${fullName}' was removed`;
-          case 'none':
-            return null;
-          case 'update':
-            return `Property '${fullName}' was updated. From ${getValueStr(elem.children[0])} to ${getValueStr(elem.children[1])}`;
-          case 'nested':
-            return iter(elem.children[0], fullName);
-          default:
-            throw new Error('Unknown record type in diff');
-        }
-      })
-      .flat(Infinity)
-      .filter((e) => e !== null);
-    return lines.join('\n');
-  };
+  const iter = (records, parent = '') => _.orderBy(records, ['key'])
+    .map((elem) => {
+      switch (elem.type) {
+        case 'add':
+          return `Property '${getFullPath(elem.key, parent)}' was added `
+            + `with value: ${getValueStr(elem.children[0])}`;
+        case 'remove':
+          return `Property '${getFullPath(elem.key, parent)}' was removed`;
+        case 'none':
+          return null;
+        case 'update':
+          return `Property '${getFullPath(elem.key, parent)}' was updated. `
+            + `From ${getValueStr(elem.children[0])} to ${getValueStr(elem.children[1])}`;
+        case 'nested':
+          return iter(elem.children[0], getFullPath(elem.key, parent));
+        default:
+          throw new Error('Unknown record type in diff');
+      }
+    })
+    .flat(Infinity)
+    .filter((e) => e !== null)
+    .join('\n');
 
   return iter(diff);
 };
